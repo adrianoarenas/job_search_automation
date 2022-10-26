97c0c359f916577593fd103ce281d88713babc6a
Adriano Arenas <adrianoarenas@gmail.com>
2022-10-26 22:04:33 +0100

    updated permissions

--- Terraform/setup.sh
+++ Terraform/setup.sh
@@ -11,5 +11,5 @@ echo "Cloning Repo"
 sudo git clone https://github.com/adrianoarenas/job_search_automation.git /home/ec2-user/job_search_automation
 
 echo "Executing /init_file.sh"
-sudo chmod +x /home/ec2-user/job_search_automation/init_file.sh
+sudo chmod 777 /home/ec2-user/job_search_automation/init_file.sh
 /home/ec2-user/job_search_automation/init_file.sh
